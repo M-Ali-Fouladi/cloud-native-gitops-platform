275fa3f6f520e15ceebd3f949cdb411d06eaab87
Mohammadali <Mohammadali@DESKTOP-599L92V>
2026-05-30 17:50:23 +0330

    'update-main'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from prometheus_client import Counter, generate_latest
 from fastapi.responses import Response
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
+
+Instrumentator().instrument(app).expose(app)
 REQUEST_COUNT = Counter(
     "app_requests_total",
     "Total Request Count"
